212dcc4ccde8a6c55dac7db856552689e79caefa
Tom Viehman <tviehman@pivotal.io>
2017-03-31 09:49:14 -0700

    Set default umask to 077
    [#142796907](https://www.pivotaltracker.com/story/show/142796907)
    
    Signed-off-by: J Evelyn <jquintana@pivotal.io>

--- bosh-stemcell/spec/bosh/stemcell/stage_collection_spec.rb
+++ bosh-stemcell/spec/bosh/stemcell/stage_collection_spec.rb
@@ -80,6 +80,7 @@ module Bosh::Stemcell
               :bosh_sudoers,
               :password_policies,
               :restrict_su_command,
+              :base_centos_umask,
               :tty_config,
               :rsyslog_config,
               :delay_monit_start,
